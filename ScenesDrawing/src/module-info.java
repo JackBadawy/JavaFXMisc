ab9dbc8962b16eb5ab1a35d625d3478e0af1c37a
JackBadawy <129567199+JackBadawy@users.noreply.github.com>
2024-06-02 18:17:09 +1000

    Add files via upload

---
+++ ScenesDrawing/src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ScenesDrawing {
+    requires javafx.controls;
+    requires javafx.graphics;
+    
+    // Export the app package to javafx.graphics module
+    exports application to javafx.graphics;
+}
