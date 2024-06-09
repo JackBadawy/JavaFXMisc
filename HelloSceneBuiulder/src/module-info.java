bdcb380e25f3aa961218ee59268f68a244e10f30
JackBadawy <129567199+JackBadawy@users.noreply.github.com>
2024-06-09 22:36:17 +1000

    sceneBuilder intro

---
+++ HelloSceneBuiulder/src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module HelloSceneBuiulder {
+    requires javafx.controls;
+    requires javafx.graphics;
+	requires javafx.fxml;
+    
+  
+    exports app to javafx.graphics;
+}
